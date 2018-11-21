a8d591e0ab1e49bbd9293405d7663f79634d6bdc
Cheng Vue <cvu3@ucsc.edu>
2018-11-21 04:03:15 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(str)
+  words_arr = str.split(" ")
+  new_words_arr = []
+
+  words_arr.each do |word|
+    new_words_arr.push(word.reverse)
+  end
+
+  return new_words_arr
+end
+  
