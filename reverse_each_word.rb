4477c991058eaa55d2f3036b578e42763d44a644
Cheng Vue <cvu3@ucsc.edu>
2018-11-21 04:12:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 def reverse_each_word(str)
   words_arr = str.split(" ")
-  #new_words_arr = []
 
   words_arr.collect do |word|
     word.reverse
   end.join(" ")
 
-  #return new_words_arr.join(" ")
 end
